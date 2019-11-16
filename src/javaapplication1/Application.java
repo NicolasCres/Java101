f10550fa3263d0508f17fd2aaca5dc6416d9f16b
nigib <nigib@DESKTOP-HI3BJ5V>
2019-11-16 15:30:43 +0000

    version 1

---
+++ src/javaapplication1/Application.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author nigib
+ */
+public class Application {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+String input= JOptionPane.showInputDialog("enter your name ");
+        System.out.println(input);
+        String name = "gabriel";
+        int number1 = 8;
+        int number2 = 5;
+
+        int answer = 6 * 85;
+
+        System.out.println(number1 * number2);
+        System.out.println(name + answer);
+        // TODO code application logic here
+        int numberA = 1;
+        int numberB = 0;
+
+        int answer01 = 1 - 0;
+
+        System.out.println(answer01);
+
+    }
+}
