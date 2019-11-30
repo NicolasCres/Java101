b6360e0c58348ff1a45107ab3f7013b557ed8f20
nigib <nigib@DESKTOP-HI3BJ5V>
2019-11-30 16:00:14 +0000

    more practise

--- src/javaapplication1/Application.java
+++ src/javaapplication1/Application.java
@@ -11,37 +11,83 @@ import javax.swing.JOptionPane;
  *
  * @author nigib
  */
-public class Application {
+public class Application
+{
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        String input = JOptionPane.showInputDialog("enter your name ");
-        System.out.println(input);
-        String name = "gabriel";
-        int number1 = 8;
-        int number2 = 5;
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args)
+	{
+		areYouOlderThan18();
+		//GreaterthanExample();
+		//MathGuessexample(args);
 
-        int answer = 6 * 85;
+	}
 
-        System.out.println(number1 * number2);
-        System.out.println(name + answer);
-        // TODO code application logic here
-        int numberA = 100;
-        int numberB = 3;
+	public static void GreaterthanExample()
 
-        int answer01 = numberA * numberB;
-        boolean Answer02 = false;
+	{
+	}
 
-        System.out.println(Answer02);
+	public static void areYouOlderThan18()
+	{
+		String ageInput = JOptionPane.showInputDialog("What age are you?");
+		int age = Integer.parseInt(ageInput);
+		if (age > 18)
+		{
+			System.out.println("you can vote");
+		} else if (age == 18)
+		{
+			System.out.println("get a job");
+		} else if (age == 12)
+		{
+			System.out.println("go to school and study");
+		} else
+		{
+			System.out.println("go back to coder dojo");
+		}
 
-        int NumberC = 8538;
-        int NumberD = 84;
+		if (age < 18 && age == 12)
+		{
+			System.out.println("last year as a child");
+		}
+		if (age < 18)
+		{
+			System.out.println("you are teenager");
+		
+		}
 
-        int Answer03 = NumberC / NumberD;
-        int NumberE = 345;
-        int NumberF = 654;
+	
+
+	public static void MathGuessexample(String[] args)
+	{
+		String input = JOptionPane.showInputDialog("enter your name ");
+		System.out.println(input);
+		String name = "Nicolas";
+		int number1 = 654;
+		int number2 = 5987;
+
+		int answer = 6 * 85;
+
+		System.out.println(number1 * number2);
+		System.out.println(name + answer);
+		// TODO code application logic here
+		int numberA = 100;
+		int numberB = 3;
+
+		int answer01 = numberA * numberB;
+		boolean Answer02 = false;
+
+		System.out.println(Answer02);
+
+		int NumberC = 8538;
+		int NumberD = 84;
+
+		int Answer03 = NumberC / NumberD;
+		int NumberE = 345;
+		int NumberF = 654;
 //        int answer = NumberE;
-    }
+	}
+
 }
